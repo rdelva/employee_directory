4c3b3cfb656e99a7c090fb5a4b12a7e44c944dd6
rdelva <roseldelva@gmail.com>
2019-10-04 01:09:33 -0400

    set up first names

--- js/script.js
+++ js/script.js
@@ -3,15 +3,27 @@
 const  xhr = new XMLHttpRequest();
 xhr.onreadystatechange = function() {
 	if(xhr.readyState === 4 && xhr.status === 200) {
+
+			let employees = JSON.parse(xhr.responseText);
+			let  statusHTML = '<ul class="employeeList">';
+			console.log(employees.length);
+
+			for(var i=0; i < employees.results.length; i++ ){
 			
-			console.log(xhr.responseText);
-			//document.getElementById('directory').innerHTML = xhr.responseText;
-			
+				statusHTML += "<li>" +  employees.results[i].name.first + "</li>";
+			}
+
+			statusHTML += '</ul>';
+			document.getElementById('directory').innerHTML = statusHTML;
+
+
+
+
 	} else {
 		console.log(xhr.statusText);
 	}
 
 };
 
-xhr.open('GET', 'https://randomuser.me/api');
+xhr.open('GET', 'https://randomuser.me/api?results=12');
 xhr.send();
