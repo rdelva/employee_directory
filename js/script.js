76161f90c4b5de677932a5de86f02fd197681002
rdelva <roseldelva@gmail.com>
2019-10-20 00:15:12 -0400

    update js

--- js/script.js
+++ js/script.js
@@ -1,38 +1,117 @@
-/*
-1. Pull Data from API to build out cards
-2. Select Card and grab the id number
-3. bA
+
+
+function  setupDirectory() {
+
+	//Establish ajax connection
+
+	const  xhr = new XMLHttpRequest();
+
+	xhr.onreadystatechange = function() {
+		if(xhr.readyState === 4 && xhr.status === 200) {
+
+				let employees = JSON.parse(xhr.responseText);
+				let  statusHTML = '';
+				
+
+				//console.log(employees);
+				console.log(employees.results);
+
+				//let {employees:'results'}  = modalList;
+				//console.log(modalList);
+
+			for(var i=0; i < employees.results.length; i++ ){
+				
+					statusHTML += `<a class="lightbox employeeCard" href="#${employees.results[i].login.username}">`; 
+					statusHTML += "<div class='card'>";
+					statusHTML += "<img class='avatar' src=" + employees.results[i].picture.large + ">"; 
+					statusHTML += "<div class='employeeInfo'>"; 
+					statusHTML += "<h2 class='name'>" + employees.results[i].name.first + " " +  employees.results[i].name.last + "</h2>";				
+					statusHTML += "<p class='email'>" +  employees.results[i].email + "</p>";
+					statusHTML += "<p class='city'>" +  employees.results[i].location.city + "</p>";
+					statusHTML += "</div>";
+					statusHTML += "</div>";
+					statusHTML += "</a>"; 	
 
 
 
+						
+			}// end of for loop for employees
 
-*/
+			displayModal(employees, null);	
+			document.getElementById('directory').innerHTML = statusHTML;
 
-//Establish ajax connection
+				
 
-const  xhr = new XMLHttpRequest();
-
-xhr.onreadystatechange = function() {
-	if(xhr.readyState === 4 && xhr.status === 200) {
-
-			let employees = JSON.parse(xhr.responseText);
-			let  statusHTML = '';
-			let  modalHTML = '';
-
-		for(var i=0; i < employees.results.length; i++ ){
+		} else {
 			
-				statusHTML += `<a class="lightbox employeeCard" href="#${employees.results[i].login.username}">`; 
-				statusHTML += "<div class='card'>";
-				statusHTML += "<img class='avatar' src=" + employees.results[i].picture.large + ">"; 
-				statusHTML += "<div class='employeeInfo'>"; 
-				statusHTML += "<h2 class='name'>" + employees.results[i].name.first + " " +  employees.results[i].name.last + "</h2>";				
-				statusHTML += "<p class='email'>" +  employees.results[i].email + "</p>";
-				statusHTML += "<p class='city'>" +  employees.results[i].location.city + "</p>";
-				statusHTML += "</div>";
-				statusHTML += "</div>";
-				statusHTML += "</a>"; 		
+		}
 
-				modalHTML += "<img class='avatar' src=" + employees.results[i].picture.large + ">"; 
+	};
+
+	xhr.open('GET', 'https://randomuser.me/api?results=12&?nat=us');
+	xhr.send();
+
+}
+	
+
+
+
+function displayModalWindow() {
+
+
+	let directory = document.querySelector('#directory');
+	let modalClose = document.querySelector('.modal-close');
+
+	directory.addEventListener('click', function(e){
+
+		let overlay = document.querySelector('overlay');
+		let employeeCard = document.querySelectorAll('employeeCard');
+		
+		let cardClicked;
+		
+
+		if (e.target.tagName == "A") {				
+			//console.log(e.target.getAttribute('href'));				
+			hidden.style.display = "block";
+			cardClicked  = e.target.getAttribute('href');
+
+			displayModal(null, cardClicked);
+				
+		}
+	});
+
+
+	let hidden = document.querySelector('.hidden');
+
+	modalClose.addEventListener('click', function(e){
+	
+		hidden.style.display = 'none';
+
+	});
+
+} // end of displayModalWindow
+		
+
+
+
+
+
+function displayModal(employees, cardClicked){
+			
+	console.log(`Test ${cardClicked}`);
+	let  modalHTML = employees;	
+	console.log(employees);
+
+	//look for the the href
+
+	for(let i = 0; i < employees.results.length; i++){
+
+	
+		if (cardClicked == employees.results[i].login.username) {
+
+			console.log('Here is my profile');
+
+				/* += "<img class='avatar' src=" + employees.results[i].picture.large + ">"; 
 				modalHTML += "<h2 class='name'>" + employees.results[i].name.first + " " +  employees.results[i].name.last + "</h2>";				
 				modalHTML += "<p class='email'>" +  employees.results[i].email + "</p>";
 				modalHTML += "<p class='city'>" +  employees.results[i].location.city + "</p>";				
@@ -43,60 +122,17 @@ xhr.onreadystatechange = function() {
 				let dateConvert = new Date(employees.results[i].dob.date);
 				//console.log(dateConvert.getMonth());
 				modalHTML += `<p>Birthday: ${dateConvert.getMonth()}/ ${dateConvert.getDay()}/${dateConvert.getYear()}</p>`;	
+				
+				document.getElementById('modal').innerHTML = modalHTML;
+*/
 
-					
-			}
 
-			displayModal(employees);	
-			document.getElementById('directory').innerHTML = statusHTML;
+		}
 
-			
-
-	} else {
-		console.log(xhr.statusText);
 	}
 
-};
+}
 
-xhr.open('GET', 'https://randomuser.me/api?results=12&?nat=us');
-xhr.send();
-
-
-
-		
-
-			let overlay = document.querySelector('overlay');
-			let directory = document.querySelector('#directory');
-			let employeeCard = document.querySelectorAll('employeeCard');
-			let hidden = document.querySelector('.hidden');
-			let modalClose = document.querySelector('.modal-close');
-			let cardClicked;
-		
-			directory.addEventListener('click', function(e){
-				if (e.target.tagName == "A") {				
-					//console.log(e.target.getAttribute('href'));				
-					hidden.style.display = "block";
-				 	cardClicked  = e.target.getAttribute('href');					
-					displayModal(cardClicked);
-				}
-			});
-
-
-			modalClose.addEventListener('click', function(e){
-				hidden.style.display = 'none';
-			});
-
-
-			function displayModal(employees, cardClicked){
-			
-				console.log(`Test ${cardClicked}`);
-				console.log(employees);
-
-				for (let i=0; i < employees.length; i++  ){
-					console.log(modalHTML);
-				}
-
-
-			}
-
-			
+setupDirectory();
+displayModalWindow();
+displayModal();
