a2288b546d6e4bea2add7379e02347bbf8890acf
rdelva <roseldelva@gmail.com>
2019-10-28 23:06:01 -0400

    add event to modal

--- js/script.js
+++ js/script.js
@@ -16,7 +16,7 @@ function  setupDirectory() {
 
 			for(var i=0; i < employees.results.length; i++ ){
 				
-					statusHTML += `<a class="lightbox employeeCard" href="#${employees.results[i].login.username}" id="${employees.results[i].login.username}">`; 
+					statusHTML += `<a  href="#${employees.results[i].login.username}" id="${employees.results[i].login.username}" class="employeeCard">`; 
 					statusHTML += "<div class='card'>";
 					statusHTML += "<img class='avatar' src=" + employees.results[i].picture.large + ">"; 
 					statusHTML += "<div class='employeeInfo'>"; 
@@ -24,8 +24,8 @@ function  setupDirectory() {
 					statusHTML += "<p class='email'>" +  employees.results[i].email + "</p>";
 					statusHTML += "<p class='city'>" +  employees.results[i].location.city + "</p>";
 					statusHTML += "</div>";
-					statusHTML += "</div>";
-					statusHTML += "</a>"; 	
+					statusHTML += `</div>`;
+					statusHTML += `</a>`; 	
 
 
 
@@ -34,6 +34,7 @@ function  setupDirectory() {
 
 			displayModalWindow(employees);	
 			document.getElementById('directory').innerHTML = statusHTML;
+			navigation();
 
 				
 
@@ -68,7 +69,9 @@ function displayModalWindow(employees) {
 		if (e.target.tagName == "A") {				
 			hidden.style.display = "block";			
 			cardClicked  = e.target.id;
+			e.target.classList.add('current');
 			displayModal(employees, cardClicked);
+			navigation(employees, cardClicked);
 				
 		}
 	});
@@ -118,6 +121,7 @@ let modalHTML = "";
 				modalHTML += `<p>Birthday: ${dateConvert.getMonth()}/ ${dateConvert.getDay()}/${dateConvert.getYear()}</p>`;				
 			
 				document.getElementsByClassName('text-container')[0].innerHTML = modalHTML;	
+
 	
 
 		}
@@ -126,8 +130,106 @@ let modalHTML = "";
 
 	}
 
+
+
+
+
 }
+
+
+
+
+
+function navigation(employees) {
+	
+	
+	const rightArrow = document.getElementById('right');
+	const leftArrow = document.getElementById('left');
+	
+	const directoryNav  =  document.querySelector('#directory');
+	//console.log(directoryNav);
+	var cards = document.querySelectorAll('a');
+	console.log(cards);
+
+
+	
+//Click right arrow
+// looks for current item
+// remove current class from current item
+// go move current class to next item
+
+
+	
+	let itemSelected;
+
+	rightArrow.addEventListener('click', function(e){		
+			
+
+		let current = directory.querySelector('.current');	
+		console.log(current);
+
+		current.classList.remove('current');	
+		let nextItem = current.nextElementSibling;
+		//console.log(nextItem);
+
+		nextItem.classList.add('current');
+
+		current = directory.querySelector('.current');	
+		//console.log(current);
+		/*let headList = current[0].parentNode;
+		console.log(headList);
+		itemSelected = current;*/
+		//itemSelected.classList.remove = current;
+
+		
+		
+	/*
+		let modalHTML ="";
+			
+			document.getElementsByClassName('text-container')[0].innerHTML = modalHTML;
+
+
+
+
+			for(let i = 0; i < items.length; i++){
+
+				if(items[i].className =='current'){
+
+			
+					//console.log(employees);
+					
+
+						modalHTML += `<img class='avatar' src="${employees.results[i].picture.large}">`; 
+						modalHTML += "<h2 class='name'>" + employees.results[i].name.first + " " +  employees.results[i].name.last + "</h2>";				
+						modalHTML += "<p class='email'>" +  employees.results[i].email + "</p>";
+						modalHTML += "<p class='city'>" +  employees.results[i].location.city + "</p>";				
+						modalHTML += "<hr>";
+						modalHTML += "<p>" +  employees.results[i].phone + "</p>";		
+						modalHTML += `<p class=address> ${employees.results[i].location.street.number} ${employees.results[i].location.street.name}, ${employees.results[i].location.city},   ${employees.results[i].location.postcode}  </p>`;
+
+						let dateConvert = new Date(employees.results[i].dob.date);
+						modalHTML += `<p>Birthday: ${dateConvert.getMonth()}/ ${dateConvert.getDay()}/${dateConvert.getYear()}</p>`;				
+					
+						document.getElementsByClassName('text-container')[0].innerHTML = modalHTML;	
+
+
+					
+				}
+
+			}
+
+			*/
+
+	
+
+		});
+
+
+}
+
+
 
 setupDirectory();
 displayModalWindow();
 displayModal();
+
