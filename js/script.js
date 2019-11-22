a48b4820abe71f6168ac36a47ce13d421ba54ce5
rdelva <rdelva@users.noreply.github.com>
2019-11-21 23:35:44 -0500

    add left navigation in modal

--- js/script.js
+++ js/script.js
@@ -43,9 +43,11 @@ function  setupDirectory() {
 	xhr.send();
 
 }
+
+
 	
 
-function filterSearch(employees){
+function filterSearch(employees, filterSearch){
 
 	let submit = document.getElementById('submit');
 
@@ -55,9 +57,11 @@ function filterSearch(employees){
 		let search = document.getElementById('search').value;
 		//console.log(search);
 		const pattern =  /\d/g; //finds a digit
-		let result = search.match(pattern);
-		//console.log(employees.results.length);
+		let result = "";
+		 result = search.match(pattern);
+	
 		if(result == null){
+			console.log(employees.results.length);
 			for(let i = 0; i < employees.results.length; i++){
 
 				//get First Name and Last Name
@@ -69,66 +73,62 @@ function filterSearch(employees){
 				let ifFound = pattern.test(name);	
 				if(ifFound !== true) {
 						document.getElementById(employees.results[i].login.username).style.display = 'none';
-				}				
+				} 				
 			}
 		}
 	});
 	
-	
-	
+filteredSearch();
 
+} 
 
+filteredSearch = function (){
+		let cards = document.querySelectorAll('.card');
+	//console.log(cards.length);
+	let cardTotal = 0;
+	//find out how many cards are not hidden
 
-		
-
-		
-		/*console.log(search);
-		
-*/
-
-
-
-
-	
-
-	//get value and assign it to varible
-	//compare varible to list
-	//if variable has letter keep it displayed if not hide
-	//if varible is found in word. 
-	//check if word doesn't have a number
+	for(let i = 0; i < cards.length; i++){
+		if(!cards[i].classList.contains('card')){
+			cardTotal++;
+		}
+		//console.log(cardTotal);
+	}
 
 	
-
-
-
 }
+
+
+
+
 
 function displayModalWindow(employees) {
 
 
-	let directory = document.querySelector('#directory');
-	let modalClose = document.querySelector('.modal-close');
+	const directory = document.getElementById('directory');
+	
+	const modalClose = document.querySelector('.modal-close');
+	const cards  = document.getElementsByClassName('card');
+	const hidden = document.querySelector('.hidden');
 
-	directory.addEventListener('click', function(e){
 
-		let overlay = document.querySelector('overlay');
-		let employeeCard = document.querySelectorAll('employeeCard');
-		
+	directory.addEventListener('click', (event) => {
+	
+
 		let cardClicked;
-		
 
-		if (e.target.tagName == "A") {				
+		if (event.target.tagName == "DIV") {				
 			hidden.style.display = "block";			
-			cardClicked  = e.target.id;
-			e.target.classList.add('current');
+			cardClicked  = event.target.id;
+			event.target.classList.add('current');
 			displayModal(employees, cardClicked);
-			//navigation(employees, cardClicked);
 				
 		}
+
+	
+
+
 	});
-
-
-	let hidden = document.querySelector('.hidden');
 
 	modalClose.addEventListener('click', function(e){
 	
@@ -145,11 +145,7 @@ function displayModalWindow(employees) {
 
 function displayModal(employees, cardClicked){	
 	
-
-	//look for the the href
-
-
-let modalHTML = "";
+	let modalHTML = "";
 	if( employees != undefined && cardClicked != undefined){
 
 		
@@ -157,35 +153,27 @@ let modalHTML = "";
 		for(let i = 0; i < employees.results.length; i++){
 
 	
-		if (cardClicked == employees.results[i].login.username) {
+			if (cardClicked == employees.results[i].login.username) {
+								
+					modalHTML += `<img class="avatar" src="${employees.results[i].picture.large}"> 
+				 					<h2 class="name"> ${employees.results[i].name.first} ${employees.results[i].name.last} </h2>				
+									<p class="email"> ${employees.results[i].email} </p>
+									<p class="city"> ${employees.results[i].location.city} </p>		
+									<hr>
+									<p>  ${ employees.results[i].phone}</p>		
+									<p class=address> ${employees.results[i].location.street.number} ${employees.results[i].location.street.name}, ${employees.results[i].location.city},   ${employees.results[i].location.postcode}  </p>`;
 
-							
-				modalHTML += `<img class='avatar' src="${employees.results[i].picture.large}">`; 
-				modalHTML += "<h2 class='name'>" + employees.results[i].name.first + " " +  employees.results[i].name.last + "</h2>";				
-				modalHTML += "<p class='email'>" +  employees.results[i].email + "</p>";
-				modalHTML += "<p class='city'>" +  employees.results[i].location.city + "</p>";				
-				modalHTML += "<hr>";
-				modalHTML += "<p>" +  employees.results[i].phone + "</p>";		
-				modalHTML += `<p class=address> ${employees.results[i].location.street.number} ${employees.results[i].location.street.name}, ${employees.results[i].location.city},   ${employees.results[i].location.postcode}  </p>`;
-
-				let dateConvert = new Date(employees.results[i].dob.date);
-				modalHTML += `<p>Birthday: ${dateConvert.getMonth()}/ ${dateConvert.getDay()}/${dateConvert.getYear()}</p>`;				
-			
-				document.getElementsByClassName('text-container')[0].innerHTML = modalHTML;	
-
-	
+									let dateConvert = new Date(employees.results[i].dob.date);
+									modalHTML += `<p>Birthday: ${dateConvert.getMonth()}/ ${dateConvert.getDay()}/${dateConvert.getYear()}</p>`;				
+				
+					document.getElementsByClassName('text-container')[0].innerHTML = modalHTML;	
+			}
 
 		}
 
 	}
 
-	}
-
-
-
-
-
-}
+}// end of displayModal
 
 
 
@@ -201,28 +189,25 @@ function navigation(employees) {
 	let itemSelected;	
 	let current;
 	let nextItem = "";
+	let prevItem ="";
 	let modalHTML ="";
 	const rightArrow = document.getElementById('right');
 	const leftArrow = document.getElementById('left');
 	const directoryNav  =  document.querySelector('#directory');
-	//console.log(directoryNav);
-	var cards = document.querySelectorAll('a');
-	//console.log(cards);
-		
+
+	
 	
 
-	rightArrow.addEventListener('click', function(e){		
-			
-		modalHTML ="";
-		
-			
-		document.getElementsByClassName('text-container')[0].innerHTML = modalHTML;	
-		
-		if(nextItem !== null) {
+	rightArrow.addEventListener('click', function(e){
 
-			current = document.querySelector('.current');	
+		modalHTML ="";	
+			
+		document.getElementsByClassName('text-container')[0].innerHTML = modalHTML;		
+		
+
+			current = document.querySelector('.current');
 			nextItem = current.nextElementSibling;
-			console.log(nextItem.id);
+			//console.log(nextItem.id);
 				if(nextItem != null) {
 					current.classList.remove('current');
 					nextItem.classList.add('current');
@@ -243,57 +228,57 @@ function navigation(employees) {
 							modalHTML += `<p>Birthday: ${dateConvert.getMonth()}/ ${dateConvert.getDay()}/${dateConvert.getYear()}</p>`;				
 						
 							//document.getElementsByClassName('text-container')[0].innerHTML = modalHTML;					
-					}
+						}
 
-			}
-				}
+					}
+				}  
 
 			document.getElementsByClassName('text-container')[0].innerHTML = modalHTML;		
-		}
-		
-
-		
-		
-	/*
-		let modalHTML ="";
-			
-			document.getElementsByClassName('text-container')[0].innerHTML = modalHTML;
-
-
-
-
-			for(let i = 0; i < items.length; i++){
-
-				if(items[i].className =='current'){
-
-			
-					//console.log(employees);
-					
-
-						modalHTML += `<img class='avatar' src="${employees.results[i].picture.large}">`; 
-						modalHTML += "<h2 class='name'>" + employees.results[i].name.first + " " +  employees.results[i].name.last + "</h2>";				
-						modalHTML += "<p class='email'>" +  employees.results[i].email + "</p>";
-						modalHTML += "<p class='city'>" +  employees.results[i].location.city + "</p>";				
-						modalHTML += "<hr>";
-						modalHTML += "<p>" +  employees.results[i].phone + "</p>";		
-						modalHTML += `<p class=address> ${employees.results[i].location.street.number} ${employees.results[i].location.street.name}, ${employees.results[i].location.city},   ${employees.results[i].location.postcode}  </p>`;
-
-						let dateConvert = new Date(employees.results[i].dob.date);
-						modalHTML += `<p>Birthday: ${dateConvert.getMonth()}/ ${dateConvert.getDay()}/${dateConvert.getYear()}</p>`;				
-					
-						document.getElementsByClassName('text-container')[0].innerHTML = modalHTML;	
-
-
-					
-				}
-
-			}
-
-			*/
-
 	
-
 		});
+
+
+
+	leftArrow.addEventListener('click', function(e){
+
+		modalHTML ="";	
+			
+		document.getElementsByClassName('text-container')[0].innerHTML = modalHTML;		
+		
+
+			current = document.querySelector('.current');
+			console.log(current);
+			prevItem = current.previousElementSibling;
+			console.log(prevItem.id);
+				if(prevItem != null) {
+					current.classList.remove('current');
+					prevItem.classList.add('current');
+
+					for(let i = 0; i < employees.results.length; i++){
+
+						if(prevItem.id == employees.results[i].login.username){
+
+							modalHTML += `<img class='avatar' src="${employees.results[i].picture.large}">`; 
+							modalHTML += "<h2 class='name'>" + employees.results[i].name.first + " " +  employees.results[i].name.last + "</h2>";				
+							modalHTML += "<p class='email'>" +  employees.results[i].email + "</p>";
+							modalHTML += "<p class='city'>" +  employees.results[i].location.city + "</p>";				
+							modalHTML += "<hr>";
+							modalHTML += "<p>" +  employees.results[i].phone + "</p>";		
+							modalHTML += `<p class=address> ${employees.results[i].location.street.number} ${employees.results[i].location.street.name}, ${employees.results[i].location.city},   ${employees.results[i].location.postcode}  </p>`;
+
+							let dateConvert = new Date(employees.results[i].dob.date);
+							modalHTML += `<p>Birthday: ${dateConvert.getMonth()}/ ${dateConvert.getDay()}/${dateConvert.getYear()}</p>`;				
+						
+							//document.getElementsByClassName('text-container')[0].innerHTML = modalHTML;					
+						}
+
+					}
+				}  
+
+			document.getElementsByClassName('text-container')[0].innerHTML = modalHTML;		
+	
+		});
+
 
 
 }
@@ -301,6 +286,7 @@ function navigation(employees) {
 
 
 setupDirectory();
-displayModalWindow();
+//displayModalWindow();
 displayModal();
 filterSearch();
+//filteredSearch();
