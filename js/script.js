dd7b69b436b745c5e5b2ea8eaeaa9fcbb0a53fa4
rdelva <rdelva@users.noreply.github.com>
2019-12-22 03:45:42 -0500

    added in displayEmployees

--- js/script.js
+++ js/script.js
@@ -1,11 +1,10 @@
 // global variables
-let​ employees = []; 
-const​ urlAPI = ​`https://randomuser.me/api/?results=12&inc=name, picture, email, location, phone, dob &noinfo &nat=US` 
-const​ gridContainer = ​document​.querySelector(​".grid-container"​); 
-const​ overlay = ​document​.querySelector(​".overlay"​); 
-const​ modalContainer = ​document​.querySelector(​".modal-content"​);
-const​ modalClose = ​document​.querySelector(​".modal-close"​);
-
+let employees = [];
+const urlAPI = "https://randomuser.me/api/?results=12&inc=name, picture, email, location, phone, dob &noinfo &nat=US";
+const  gridContainer = document.querySelector(".grid-container");
+const overlay = document.querySelector(".overlay");
+const modalContainer = document.querySelector(".modal-content");
+const modalClose  = document.querySelector(".modal-close");
 
 
 fetch(urlAPI)
@@ -15,6 +14,31 @@ fetch(urlAPI)
 	.catch(err => console.log(err))
 
 
-displayEmployees () => {
+function displayEmployees(employeeData){
+
+	employees = employeeData;
+	let employeesHTML = "";
+
+	employees.forEach((employee, index) => {
+		let name = employee.name;
+		let email = employee.email;
+		let city = employee.location.city;
+		let picture = employee.picture;
+
+	});
+
+	employeesHTML += `
+		<div class="card" data-index="${index}">
+			<img class="avatar" src="${picture.large}"/>
+			<div class="text-container">
+				<h2 class="name">${name.first} ${name.last}</h2>
+				<p class="email">${email}</p>
+				<p class="address">${city}</p>
+			</div>
+		</div>
+	`;
+
+	gridContainer.innerHTML = employeesHTML;
 
 }
+
