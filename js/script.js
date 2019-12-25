4c2f7fc506ef36625bfedb0613b351eb3426bf52
rdelva <rdelva@users.noreply.github.com>
2019-12-25 07:02:23 -0500

    add in display modal and event listener

--- js/script.js
+++ js/script.js
@@ -46,3 +46,42 @@ function displayEmployees(employeeData){
 
 }
 
+
+function displayModal(index){
+
+	let { name, dob, phone, email, location: { city, street, state, postcode}, picture } = employees[index];
+
+	let date = new Date(dob.date);
+
+	const modalHTML = `
+		<img class ="avatar" src="${picture.large} " />
+		<div>
+			<h2 class="name">${name.first} ${name.last}</h2>
+			<p class="email">${email}</p>
+			<p class="address">${city}</p>
+			<hr/>
+			<p>${phone}</p>
+			<p class="address"> ${street}, ${state} ${postcode}</p>
+			<p>Birthday: ${date.getMonth()}/${date.getDate()}/ ${date.getFullYear()}</p>
+		</div>
+
+	`;
+
+	overlay.classList.remove("hidden");
+	modalContainer.innerHTML = modalHTML;
+}
+
+
+gridContainer.addEventListener('click', e => {
+
+	//only targets the cards in employee directory
+	if(e.target !== gridContainer){
+
+		const card = e.target.closest(".card");
+		const index = card.getAttribute("data-index");
+
+		displayModal(index);
+
+	}
+
+});
